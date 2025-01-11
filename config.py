77aa0f3d36df398c7eb2c5164dcaedd2defd3cfa
Viratrajesh <115334651+Viratrajesh@users.noreply.github.com>
2025-01-11 07:31:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     API_HASH = environ.get("API_HASH", "0605c5395b91ead763072251e20c3417")
     BOT_TOKEN = environ.get("BOT_TOKEN", "8109432845:AAE-hrRVPKeIERtNMb_3sNt1a05-ALJI-bI") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://amdmoviess18:AlETsgSChcv9UfrL@cluster0.924oz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://amdmoviess18:AlETsgSChcv9UfrL@cluster0.924oz.mongodb.net/")
     DATABASE_NAME = environ.get("DATABASE_NAME", "amdmoviess18")
     BOT_OWNER = int(environ.get("BOT_OWNER", "5371238852"))
 
