b44e0f606d115dea12b14d9b9a8e0006316959d6
Viratrajesh <115334651+Viratrajesh@users.noreply.github.com>
2025-01-14 07:01:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ class Config:
     API_HASH = environ.get("API_HASH", "0605c5395b91ead763072251e20c3417")
     BOT_TOKEN = environ.get("BOT_TOKEN", "8109432845:AAE-hrRVPKeIERtNMb_3sNt1a05-ALJI-bI") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://amdmoviess18:AIRTsgSChcv9UfrL@cluster0.elblpo5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "amdmoviess18")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://viratrajesh83:Rajesh@18@cluster0.qeqrf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "viratrajesh83")
     BOT_OWNER = int(environ.get("BOT_OWNER", "5371238852"))
 
 # Don't Remove Credit Tg - @VJ_Botz
