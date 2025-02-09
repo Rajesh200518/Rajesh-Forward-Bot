41da917bd82eb126fed39ec4cfa16b305b6a466c
Ganesh17032009ramesh <ganeshramesh173@gmail.com>
2025-02-09 19:16:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = int(environ.get("API_ID", "20902603"))
+    API_HASH = environ.get("API_HASH", "79e5caa103a9e9fb0183390b4800845d")
     BOT_TOKEN = environ.get("BOT_TOKEN", "8109432845:AAE-hrRVPKeIERtNMb_3sNt1a05-ALJI-bI") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://viratrajesh83:Rajesh@18@cluster0.qeqrf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
